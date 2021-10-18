3b0a7344747479e4fe0555f272a00f458f6c681f
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-18 10:58:41 -0300

    desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,22 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: {
+        $exists: true,
+        $not: { $in: [""] },
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+    },
+  },
+]);
